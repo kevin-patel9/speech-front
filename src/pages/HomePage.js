2f7048f684e348d4a69ee5d5093c26ea56721007
unknown <kevinpatel0129@gmail.com>
2024-10-01 22:50:43 +0530

    fixed bug to redirect to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,12 +15,7 @@ const HomePage = () => {
 
   useEffect(() => {
     if (!isLoggedIn) navigate("/login");
-
-    // to avoid hard refresh
-    setTimeout(() => {
-      setRefresh(!refresh);
-    },300)
-  }, [isLoggedIn]);
+  }, []);
 
   const handleLogout = () => {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;";
